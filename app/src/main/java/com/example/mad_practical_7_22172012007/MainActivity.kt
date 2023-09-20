a69dbcb1a9852c76ffe3d7c44a81b4b9980f34d0
student <student@gnu.ac.in>
2023-09-20 08:51:31 +0530

    hey

---
+++ app/src/main/java/com/example/mad_practical_7_22172012007/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mad_practical_7_22172012007
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       val mediaController = MediaController(this)
+        val uri: Uri = Uri.parse("android.resource://" + packageName +"/" +R.raw.thestoryoflight)
+        val myvedioview =findViewById<VideoView>(R.id.videoView)
+        
+
+
+    }
+}
