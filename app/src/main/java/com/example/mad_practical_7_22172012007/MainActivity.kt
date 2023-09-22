f3373c771e008270374dc8025046977169433f29
student <student@gnu.ac.in>
2023-09-22 12:33:58 +0530

    hey

--- app/src/main/java/com/example/mad_practical_7_22172012007/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_22172012007/MainActivity.kt
@@ -10,10 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       val mediaController = MediaController(this)
+        val mediaController = MediaController(this)
         val uri: Uri = Uri.parse("android.resource://" + packageName +"/" +R.raw.thestoryoflight)
-        val myvedioview =findViewById<VideoView>(R.id.videoView)
-        
+        val myvideoview =findViewById<VideoView>(R.id.videoView)
+        mediaController.setAnchorView(myvideoview)
+        myvideoview.setVideoURI(uri)
+        myvideoview.requestFocus()
+        myvideoview.start()
+
+
 
 
     }
--- app/src/main/java/com/example/mad_practical_7_22172012007/YoutubeActivity.kt
+++ app/src/main/java/com/example/mad_practical_7_22172012007/YoutubeActivity.kt
@@ -2,10 +2,14 @@ package com.example.mad_practical_7_22172012007
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class YoutubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+        val youtubeId = "fjn9B64Znrk"
+        val webView = findViewById<WebView>(R.id.webView)
+
     }
 }
